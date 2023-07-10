7944e9caf7b1e5a78c459bde2f5b839c2f5cf956
Shagun <jaiswalshagun79@gmail.com>
2023-07-10 23:01:00 +0530

    first commit

---
+++ quiz.js
@@ -0,0 +1,17 @@
+let name = ["shagun", "sajal", "anykaa"];
+
+let quiz = [
+  {
+    question: "What is the population of America?",
+    answer: [5, 10, 15, 20],
+  },
+  {
+    question: "What is the most faviourite place in America for tourism",
+
+    answer: ["Atlantic", "Gulf"],
+  },
+  {
+    question: "What is the population of America?",
+    answer: [5, 10, 15, 20],
+  },
+];
